5bdf7205377ea5015ce69c480116ae5e63430efe
billy.hale <billy.hale@gce.com>
2023-11-14 14:19:05 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import core from '@actions/core';
+
+const url = core.getInput('url');
+const env = core.getInput('env');
+
+await core.summary
+  .addHeading('Blue Green Deployment Summary')
+  .addRaw('This action will *always* deploy to the inactive environment.',true)
+  .addRaw(`## Deployed to: ${env}`)
+  .addRaw(`URL: [${url}](${url})`)
+  .addRaw('It may take up to 5 minutes for ECS to deploy the container and any launch commands to complete.')
